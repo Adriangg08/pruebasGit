280c7910c388234ca2ee26915c3abd8f57889b4e
Your Name <you@example.com>
2022-10-25 09:41:56 +0200

    tercer commit

--- src/pruebasGit/Coches.java
+++ src/pruebasGit/Coches.java
@@ -4,4 +4,18 @@ public class Coches {
 
 	private int puertas;
 	private int caballos;
+	
+	public int getPuertas() {
+		return puertas;
+	}
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
+	public int getCaballos() {
+		return caballos;
+	}
+	public void setCaballos(int caballos) {
+		this.caballos = caballos;
+	}
+	
 }
